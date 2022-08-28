1785e1977a330ae15a6a41506793aa576e607534
rohanwinsor <rohan.w.charles@gmail.com>
2022-08-28 09:05:20 +0530

    removed unwanted comments

--- basicgrad/nn.py
+++ basicgrad/nn.py
@@ -5,8 +5,6 @@ class Neuron:
     def __init__(self, nin, nonlin=True):
         self.w = [Value(random.uniform(-1,1), label=f'w{i}') for i in range(nin)]
         self.b = Value(random.uniform(-1,1), label='b')
-        # self.w = [Value(1.8)]
-        # self.b = Value(32.0)
         self.nonlin = nonlin
 
     def __call__(self, x):
